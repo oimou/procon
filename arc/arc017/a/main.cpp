30e09ca077c6e51ea78df0a2264a781a36663080
oimou <s1190013@gmail.com>
2019-10-16 01:43:03 +0900

    arc017 A necessary and sufficient condition

--- arc/arc017/a/main.cpp
+++ arc/arc017/a/main.cpp
@@ -23,7 +23,7 @@ int main () {
 }
 
 /*
-N$を2以上の整数とする。
+$N$を2以上の整数とすると，$N$は素数あるいは合成数のいずれかである。
 $N$が合成数ならば，2つの正整数$n_1, n_2\ (2 \leq n_1 \leq n_2 \leq N - 1)$を用いて
 $N = n_1 n_2$と表すことができ，
 $\lceil \sqrt{N} \rceil \geq \sqrt{N}$より$\lceil \sqrt{N} \rceil^2 \geq N$
@@ -39,10 +39,14 @@ $\lceil \sqrt{N} \rceil \geq \sqrt{N}$より$\lceil \sqrt{N} \rceil^2 \geq N$
 
 したがって，次が成り立つ：
 \[
-    \mbox{$N$が合成数} \Longrightarrow \mbox{$N$は$\lceil \sqrt{N} \rceil$以下の約数を持つ}
+    \mbox{$N$が合成数} \Longrightarrow \mbox{$N$は$2 \leq n \leq \lceil \sqrt{N} \rceil$なる約数$n$を持つ}
 \]
-対偶より，
+また，素数の定義から明らかに次が成り立つ：
 \[
-    \mbox{$N$が$\lceil \sqrt{N} \rceil$以下の約数を持たない} \Longrightarrow \mbox{$N$は素数} 
+    \mbox{$N$が素数} \Longrightarrow \mbox{$N$は$2 \leq n \leq \lceil \sqrt{N} \rceil$なる約数$n$を持たない}
+\]
+したがって，次が成り立つ：
+\[
+    \mbox{$N$が素数} \Longleftrightarrow \mbox{$N$は$2 \leq n \leq \lceil \sqrt{N} \rceil$なる約数$n$を持たない}
 \]
 */
