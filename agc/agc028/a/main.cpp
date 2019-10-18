5e0e538e703fa389160427141d2f52976f62c06f
oimou <s1190013@gmail.com>
2019-10-18 14:04:31 +0900

    AGC028 A: int to long long

--- agc/agc028/a/main.cpp
+++ agc/agc028/a/main.cpp
@@ -3,19 +3,20 @@
 #include <boost/math/common_factor_rt.hpp>
 
 using namespace std;
+using Integer = long long;
 
 int main () {
-  int N, M;
+  Integer N, M;
   string S, T;
 
   cin >> N >> M >> S >> T;
 
-  int l = boost::math::lcm(N, M);
-  int g = boost::math::gcd(N, M);
-  int N_ = N / g;
-  int M_ = M / g;
+  Integer l = boost::math::lcm(N, M);
+  Integer g = boost::math::gcd(N, M);
+  Integer N_ = N / g;
+  Integer M_ = M / g;
 
-  for (int d = 0; d < g; d++) {
+  for (Integer d = 0; d < g; d++) {
     if (S.at(N_ * d) != T.at(M_ * d)) {
       cout << -1 << "\n";
       return 0;
