32ba066e532152d10342ad20d4b5843fa9483e40
oimou <s1190013@gmail.com>
2019-10-18 18:44:42 +0900

    ABC116 B

---
+++ abc/abc116/b/all.h
@@ -0,0 +1,8 @@
+#include <utility>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/graph_utility.hpp>
