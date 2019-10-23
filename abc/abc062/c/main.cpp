a1f165fec142d12ec1506c69d820e64b3e5505b9
oimou <s1190013@gmail.com>
2019-10-24 01:18:36 +0900

    ABC062 C: Fixes #11

--- abc/abc062/c/main.cpp
+++ abc/abc062/c/main.cpp
@@ -3,35 +3,26 @@
 using namespace std;
 using Integer = long long;
 
-Integer min_i (Integer W, Integer H) {
-  return min(W, H);
+Integer min_i (Integer H, Integer W) {
+  return min(H, W);
 }
 
-Integer min_t (Integer W, Integer H) {
-  Integer res = 1e10;
-
-  for (Integer l = H / 2; l < H; l++) {
-    Integer S_head = W * (H - l);
-    Integer S_larger  = (Integer) (l *  ceil((double)W / 2));
-    Integer S_smaller = (Integer) (l * floor((double)W / 2));
-    Integer S_max = max(S_head, S_larger);
-    Integer S_min = min(S_head, S_smaller);
-    Integer dS = S_max - S_min;
-
-    if (dS < res) res = dS;
-    if (S_head < S_smaller) break;
-  }
-
-  return res;
+Integer min_t (double H, double W) {
+  Integer h1 =  ceil( W * H / ( W + floor(W / 2) ) );
+  Integer h2 = floor( W * H / ( W +  ceil(W / 2) ) );
+  return min(
+    h1 * ceil(W / 2) - W * (H - h1),
+    W * (H - h2) - h2 * floor(W / 2)
+  );
 }
 
 int main () {
   Integer H, W;
   cin >> H >> W;
 
-  int ans = (H % 3 == 0 || W % 3 == 0)
+  Integer ans = (H % 3 == 0 || W % 3 == 0)
     ? 0
-    : min(min_i(W, H), min(min_t(W, H), min_t(H, W)));
+    : min(min_i(H, W), min(min_t(H, W), min_t(W, H)));
 
   cout << ans << "\n";
 }
