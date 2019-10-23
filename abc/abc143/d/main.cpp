441c4feefbda2d1c13a233aee0277b568f684106
oimou <s1190013@gmail.com>
2019-10-23 15:04:55 +0900

    ABC143 D: Fix a proof

--- abc/abc143/d/main.cpp
+++ abc/abc143/d/main.cpp
@@ -1,34 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <tuple>
-#include <iterator>
-#include <functional>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int input () { int x; cin >> x; return x; }
 
 int main () {
-  int N;
+  int N, count;
+  vector<int> L;
+
   cin >> N;
 
-  vector<int> L;
   for (int i = 0; i < N; i++) L.push_back(input());
+
   sort(L.begin(), L.end());
 
-  for (auto & i : L) cout << i << " "; cout << endl;
-
-  int count = 0;
-  for (auto ic = L.end() - 1; ic >= L.begin() + 2; ic--) {
-    for (auto ib = ic - 1; ib >= L.begin() + 1; ib--) {
-      if (*ic >= 2 * *ib) continue;
-
-      auto ix = lower_bound(L.begin(), ib, *ic - *ib, less<int>());
-      count += distance(ix, ib);
-
-      printf("ic-ib:%d ix:%d ib:%d ic:%d\n", *ic-*ib, *ix, *ib, *ic);
+  for (auto ib = L.end() - 1; ib >= L.begin(); ib--) {
+    for (auto ia = ib - 1; ia >= L.begin(); ia--) {
+      auto ic_upper_limit = lower_bound(ib, L.end(), *ia + *ib);
+      count += distance(ib, ic_upper_limit) - 1;
     }
   }
 
