dbc8701d77ed9a852e831b502954bc18d9fcbc56
oimou <s1190013@gmail.com>
2019-10-26 23:50:09 +0900

    ABC108 A: Done

---
+++ abc/abc108/a/README.md
@@ -0,0 +1,1 @@
+https://atcoder.jp/contests/abc108/tasks/abc108_a
---
+++ abc/abc108/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+  double K;
+  cin >> K;
+  cout << floor(K / 2) * ceil(K / 2) << "\n";
+}
