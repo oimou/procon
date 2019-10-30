6670f5c2c5028fa3c6c66b227467f6afde754b5b
oimou <s1190013@gmail.com>
2019-10-30 22:35:42 +0900

    ALDS1_3_C: Done

--- aoj/ALDS1_3_C/main.cpp
+++ aoj/ALDS1_3_C/main.cpp
@@ -20,9 +20,30 @@ void insert (Integer key) {
   x->prev = nil;
 }
 
-Node * list_search (Integer key) {}
+Node * list_search (Integer key) {
+  Node * cur = nil->next;
+  while (cur->key != key) {
+    if (cur == nil) break;
+    cur = cur->next;
+  }
+  return cur;
+}
 
-void delete_node (Node * x) {}
+void delete_node (Node * x) {
+  if (x == nil) return;
+
+  x->prev->next = x->next;
+  x->next->prev = x->prev;
+  free(x);
+}
+
+void delete_first () {
+  delete_node(nil->next);
+}
+
+void delete_last () {
+  delete_node(nil->prev);
+}
 
 void print_list () {
   Node * cur = nil->next;
@@ -48,14 +69,18 @@ int main () {
   nil->prev = nil;
 
   while (n--) {
-    char command[11];
+    char command[20];
     Integer key;
     scanf("%s %d", command, &key);
 
-    if ("insert" == string(command)) {
+    if (string(command) == "insert") {
       insert(key);
-    } else if ("delete" == string(command)) {
+    } else if (string(command) == "delete") {
       delete_node(list_search(key));
+    } else if (string(command) == "deleteFirst") {
+      delete_first();
+    } else if (string(command) == "deleteLast") {
+      delete_last();
     }
   }
 
