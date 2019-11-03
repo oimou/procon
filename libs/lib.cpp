eb5bc16b7a818fa34c74e1cf035ad10091ff812f
oimou <s1190013@gmail.com>
2019-11-03 22:02:35 +0900

    Fix some things

--- libs/lib.cpp
+++ libs/lib.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 using Integer = long long;
 
+/**
+ * amazingly fast IO
+ * NOTE: fread_unlocked does not work on macOS.
+ */
+const int cm = 1 << 17;
+char cn[cm], * ci = cn + cm, ct;
+inline char getcha() {
+	if (ci - cn == cm) { fread_unlocked(cn, 1, cm, stdin); ci = cn; }
+	return *ci++;
+}
+inline int getint() {
+	int A = 0;
+	if (ci - cn + 16 > cm) while ((ct = getcha()) >= '0') A = A * 10 + ct - '0';
+	else while ((ct = *ci++) >= '0') A = A * 10 + ct - '0';
+	return A;
+}
+
+/**
+ * swap values
+ */
 template <typename T>
 void swap (T * a, T * b) {
   T tmp = *a;
@@ -10,11 +30,17 @@ void swap (T * a, T * b) {
   *b = tmp;
 }
 
+/**
+ * print an array in a row
+ */
 template <typename T>
 void print_array (T arr[], size_t n) {
   for (int i = 0; i < n; i++) printf("%d%c", arr[i], (i == n - 1) ? '\n' : ' ');
 }
 
+/**
+ * Sieve of Eratosthenes
+ */
 void sieve (bool is_prime[], Integer N) {
   Integer sqrt_N = sqrt(N);
 
--- templates/main.cpp
+++ templates/main.cpp
@@ -1,3 +1,5 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
 #include <bits/stdc++.h>
 
 using namespace std;
