5aa62437e30493e1d747ffe34649a3a0d97ee6a2
oimou <s1190013@gmail.com>
2020-03-03 22:53:25 +0900

    ABC157 D: Add a skeleton of Union-FInd forest

--- abc/abc157/d/main.cpp
+++ abc/abc157/d/main.cpp
@@ -1,105 +1,59 @@
 #pragma GCC optimize ("O3")
 #pragma GCC target ("avx")
 #include <bits/stdc++.h>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/dijkstra_shortest_paths.hpp>
-#include <boost/assign/list_of.hpp>
 
 using namespace std;
 
-struct myGraphProperty
-{
-  std::string name;
+struct Node {
+  Node * parent;
 };
 
-struct myVertexProperty
-{
- std::string name;
-};
+struct UnionFindForest {
+  map<int, Node *> nodes;
 
-struct myEdgeProperty
-{
-  std::string name;
-};
+  UnionFindForest() = default;
 
-using Graph = boost::adjacency_list<
-  boost::vecS,
-  boost::vecS,
-  boost::undirectedS,
-  myVertexProperty,
-  myEdgeProperty,
-  myGraphProperty
->;
+  void make_set (int i) {
+    Node * node = (Node *) malloc(sizeof(Node));
+    nodes.insert(make_pair(i, node));
+    node->parent = node;
+  }
+
+  void unite (int ix, int iy) {
+    Node * x = nodes.at(ix);
+    Node * y = nodes.at(iy);
+    Node * xRoot = find(x);
+    Node * yRoot = find(y);
+    xRoot->parent = yRoot;
+  }
+
+  Node * find (Node * x) {
+    if (x->parent == x) {
+      return x;
+    } else {
+      return find(x->parent);
+    }
+  }
+
+  bool compare (int ix, int iy) {
+    Node * x = nodes.at(ix);
+    Node * y = nodes.at(iy);
+    return find(x) == find(y);
+  }
+};
 
 /**
  * main
  */
 int main () {
-  int N, M, K;
-  cin >> N >> M >> K;
+  UnionFindForest uff;
 
-  Graph graph;
-  using Vertex = decltype(graph)::vertex_descriptor;
-  using Edge = decltype(graph)::edge_descriptor;
+  uff.make_set(1);
+  uff.make_set(2);
 
-  vector<Vertex> vertices;
-  map<pair<int, int>, Edge> edges;
+  cout << uff.compare(1, 2) << '\n';
 
-  for (int i = 0; i <= N; i++) {
-    Vertex v = boost::add_vertex(graph);
-    vertices.push_back(v);
-  }
+  uff.unite(1, 2);
 
-  for (int i = 0; i < M; i++) {
-    int A, B;
-    scanf("%d %d", &A, &B);
-
-    Vertex va = vertices.at(A);
-    Vertex vb = vertices.at(B);
-    Edge e = std::get<0>(boost::add_edge(va, vb, graph));
-    edges.insert(make_pair(make_pair(A, B), e));
-  }
-
-  for (int i = 0; i < K; i++) {
-    int C, D;
-    scanf("%d %d", &C, &D);
-
-    Vertex vc = vertices.at(C);
-    Vertex vd = vertices.at(D);
-
-    try {
-      Edge e = edges.at(make_pair(C, D));
-      boost::remove_edge(e, graph);
-    } catch(std::out_of_range&) {
-      // do nothing
-    }
-
-    try {
-      Edge e = edges.at(make_pair(D, C));
-      boost::remove_edge(e, graph);
-    } catch(std::out_of_range&) {
-      // do nothing
-    }
-  }
-
-  for (int i = 1; i <= N; i++) {
-    Vertex from = vertices.at(i);
-    int n_friends = 0;
-
-    for (int j = 1; j <= N; j++) {
-      if (i == j) continue;
-
-      Vertex to = vertices.at(j);
-
-      // TODO
-
-      n_friends++;
-    }
-
-    printf("%d", n_friends);
-
-    if (i != N) printf(" ");
-  }
-
-  printf("\n");
+  cout << uff.compare(1, 2) << '\n';
 }
