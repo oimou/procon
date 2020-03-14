952d9621b9059ac0cb93d4042be92236d54e9d2c
oimou <s1190013@gmail.com>
2020-03-15 00:39:19 +0900

    panasonic2020 E: Refer to the editorial

--- others/panasonic2020/e/main.cpp
+++ others/panasonic2020/e/main.cpp
@@ -4,6 +4,16 @@
 using namespace std;
 using ll = long long;
 
+static const int M = 2000;
+
+vector<bool> ab(100000, false);
+vector<bool> ac(100000, false);
+vector<bool> bc(100000, false);
+
+bool match (char x, char y) {
+  return (x == '?' || y == '?' || x == y);
+}
+
 /**
  * main
  */
@@ -12,17 +22,39 @@ int main () {
   string b; cin >> b;
   string c; cin >> c;
 
-  int la = a.size();
-  int lb = b.size();
-  int lc = c.size();
+  int la = int(a.length());
+  int lb = int(b.length());
+  int lc = int(c.length());
 
-  REP(i, 0, la + lb + 1) {
-    int p_ba = i - lb;
-    int l = min(p_ba + 0,  0);
-    int r = max(p_ba + lb, la);
-    bool is_valid = true;
+  REP(i, 0, la)
+    REP(j, 0,lb)
+      if (!match(a[i], b[j]))
+        ab[i - j + 50000] = true;
 
-    REP(j, 0, r - l) {
+  REP(i, 0, la)
+    REP(j, 0,lc)
+      if (!match(a[i], c[j]))
+        ac[i - j + 50000] = true;
+
+  REP(i, 0, lb)
+    REP(j, 0,lc)
+      if (!match(b[i], c[j]))
+        bc[i - j + 50000] = true;
+
+  int ans = 3 * M;
+
+  for (int i = -2 * M; i <= 2 * M; i++) {
+    for (int j = -2 * M; j <= 2 * M; j++) {
+      if (!(ab[i + 50000] || ac[j + 50000] || bc[j - i + 50000])) {
+        int L = min(0, min(i, j));
+        int R = max(la, max(lb + i, lc + j));
+
+        ans = min(ans, R - L);
+      }
     }
   }
+
+  cout << ans << endl;
+
+  return 0;
 }
